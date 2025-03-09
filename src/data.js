e16a367048ab0fa8623f1a58c97d0b8752904e67
DevangNegi <130244176+DevangNegi@users.noreply.github.com>
2025-03-09 18:01:39 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -21,7 +21,7 @@ export const storeProducts = [
     title: "Samsung S7",
     img: "img/product-2.png",
     price: 16,
-    company: "SAMSUNG",
+    company: "REALME",
     info:
       "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
     inCart: false,
